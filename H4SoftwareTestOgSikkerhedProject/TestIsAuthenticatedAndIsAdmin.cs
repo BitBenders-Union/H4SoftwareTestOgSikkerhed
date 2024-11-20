a95bdce291cef962cdd450a67e9224bedff63e53
Jonas Østergaard <Jonasoe@hotmail.com>
2024-11-20 09:57:24 +0100

    TesIsAuthenticatedAndIsAdmin
    Role
    PasswordPolicy

---
+++ H4SoftwareTestOgSikkerhedProject/TestIsAuthenticatedAndIsAdmin.cs
@@ -0,0 +1,36 @@
+using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace H4SoftwareTestOgSikkerhedProject;
+
+public class TestIsAuthenticatedAndIsAdmin
+{
+    [Fact]
+    public void IsAuthenticatedAndIsAdminTest()
+    {
+        // Arrange
+        using var ctx = new TestContext();
+        var authContext = ctx.AddTestAuthorization();
+        authContext.SetAuthorized("test", AuthorizationState.Authorized);
+        authContext.SetRoles("Admin");
+
+        // Act
+        var cut = ctx.RenderComponent<H4SoftwareTestOgSikkerhed.Components.Pages.Home>();
+
+        // Assert
+        cut.MarkupMatches(@"<h1>Hello, world!</h1>
+                            <p>Welcome to your new app.</p>
+                            	<div>
+		                            <p>You are Authenticated!</p>
+	                            </div>
+		                        <div>
+			                        <p>You are an Admin!</p>
+		                        </div>");
+
+
+    }
+
+
+}
