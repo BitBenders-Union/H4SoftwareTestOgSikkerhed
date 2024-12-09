160a701dafec223a39866f9f39fa2b365f8f6bbb
ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>
2024-12-09 10:30:58 +0100

    bugfix
    Co-Authored-By: Dennis Daniel Bygvraae Paaske <102860689+Denpas404@users.noreply.github.com>

--- H4SoftwareTestOgSikkerhed/Program.cs
+++ H4SoftwareTestOgSikkerhed/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddAuthentication(options =>
 
 // if os is linux use sqlite
 var connectionString = string.Empty;
+var connectionString2 = string.Empty;
 
 if (OperatingSystem.IsLinux())
 {
@@ -44,9 +45,9 @@ else
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseSqlServer(connectionString));
     builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-    connectionString = builder.Configuration.GetConnectionString("ToDoConnection") ?? throw new InvalidOperationException("Connection string 'ToDoConnection' not found.");
+    connectionString2 = builder.Configuration.GetConnectionString("ToDoConnection") ?? throw new InvalidOperationException("Connection string 'ToDoConnection' not found.");
     builder.Services.AddDbContext<ToDoDBContext>(options =>
-        options.UseSqlServer(connectionString));
+        options.UseSqlServer(connectionString2));
     builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 }
 
