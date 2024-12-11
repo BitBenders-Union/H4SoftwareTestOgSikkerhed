49574bc0ab4d1100682fe05902c3519cb2a0a483
Jonas Østergaard <Jonasoe@hotmail.com>
2024-12-11 10:50:06 +0100

    keeeeeeys

--- H4SoftwareTestOgSikkerhed/Components/Services/AsymetricEncryptionService.cs
+++ H4SoftwareTestOgSikkerhed/Components/Services/AsymetricEncryptionService.cs
@@ -1,19 +1,39 @@
-﻿using System.Security.Cryptography;
+﻿using Org.BouncyCastle.Crypto;
+using System.Security.Cryptography;
 using System.Text;
 namespace H4SoftwareTestOgSikkerhed.Components.Services;
 
 public class AsymetricEncryptionService
 {
-    private readonly string apiUrl = "https://localhost:5001/api/Encryption";
+    private readonly string apiUrl = "https://localhost:7020/api/AsymetricEncryption";
     private string PrivateKey { get; }
     public string PublicKey { get; }
+
     public AsymetricEncryptionService()
     {
-        using (RSACryptoServiceProvider rsa = new())
+        string privateKeyPath = "privateKey.txt";
+        string publicKeyPath = "publicKey.txt";
+
+        RSACryptoServiceProvider rsa = new();
+
+        //if (!File.Exists(privateKeyPath) || !File.Exists(publicKeyPath))
+        if (!File.Exists(privateKeyPath))
         {
-            PrivateKey = rsa.ToXmlString(true);
-            PublicKey = rsa.ToXmlString(false);
+
+                PrivateKey = rsa.ToXmlString(true);
+                PublicKey = rsa.ToXmlString(false);
+
+                SaveKey(privateKeyPath, PrivateKey);
+                SaveKey(publicKeyPath, PublicKey);
+
         }
+        else
+        {
+            PrivateKey = LoadKey(privateKeyPath);
+            PublicKey = LoadKey(publicKeyPath);
+        }
+
+
     }
 
     public string Encrypt(string data)
@@ -27,14 +47,15 @@ public class AsymetricEncryptionService
         }
     }
 
-    public string Decrypt(string data)
+    public string DecryptAsymetric(string valueToDecrypt)
     {
-        using (RSACryptoServiceProvider rsa = new())
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
         {
             rsa.FromXmlString(PrivateKey);
-            byte[] dataBytes = Convert.FromBase64String(data);
-            byte[] decryptedBytes = rsa.Decrypt(dataBytes, false);
-            return Encoding.UTF8.GetString(decryptedBytes);
+
+            byte[] valueToEncryptAsBytes = Convert.FromBase64String(valueToDecrypt);
+            byte[] encryptedValue = rsa.Decrypt(valueToEncryptAsBytes, false);
+            return System.Text.Encoding.UTF8.GetString(encryptedValue);
         }
     }
 
@@ -47,5 +68,20 @@ public class AsymetricEncryptionService
         using HttpClient httpClient = new();
         HttpResponseMessage response = await httpClient.PostAsync(apiUrl, payload);
         return response.Content.ReadAsStringAsync().Result;
+    }
+
+    // store my keys
+    public static void SaveKey(string filename, string content)
+    {
+        var bytes = Encoding.ASCII.GetBytes(content);
+        using var fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+        fs.Write(bytes, 0, bytes.Length);
+    }
+
+    // load key
+    public static string LoadKey(string filename)
+    {
+        var bytes = File.ReadAllBytes(filename);
+        return Encoding.ASCII.GetString(bytes);
     }
 }
